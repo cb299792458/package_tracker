c2515ef8c6da986cc3238ec4c58c697d01d2b0ff
brian <brianrlam@gmail.com>
2023-07-31 12:42:13 -0400

    make package form and display

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_migrate import Migrate
 from .config import Config
 from .shipping_form import ShippingForm
+from .models import db, Package
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate=Migrate(app,db)
 
-@app.route('/',methods=['GET'])
-def index():
-    return 'Package Tracker Home'
+@app.route('/')
+def root_endpoint():
+    packages = Package.query.all()
+    return render_template('package_status.html',packages=packages)
 
 @app.route('/new_package',methods=['GET','POST'])
 def new_package():
     form = ShippingForm()
 
     if form.validate_on_submit():
-
-        return redirect(url_for('.index'))
+        data = form.data
+        new_package = Package(sender=data["sender"],
+                              recipient=data["recipient"],
+                              origin=data["origin"],
+                              destination=data["destination"],
+                              location=data["origin"])
+        db.session.add(new_package)
+        db.session.commit()
+        return redirect('/')
 
     return render_template('shipping_request.html',form=form)
+
--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ from os import environ
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
